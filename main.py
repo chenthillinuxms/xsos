892c20a3b605f2285657e40a3c831ee2be36b59a
chenthillinuxms <87596132+chenthillinuxms@users.noreply.github.com>
2023-02-05 17:31:03 +0530

    Update main.py
    Correction related to path fetching is down on the script

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import os
 import subprocess
 import sys
 import re
 import glob
+import gzip
+import shutil
+
 
 print('cmd entry:', sys.argv)
 
@@ -36,9 +39,18 @@ else:
 
 unxz_tar_file_name = sys.argv[1]
 sosreport_directory = unxz_tar_file_name.split(".tar",1)[0]
-tar_file_name  = unxz_tar_file_name.split(".xz",1)[0]
-print(tar_file_name)
 print(sosreport_directory)
+sosreport_var_log_directory = "{}/var/log".format(sosreport_directory)
+print(sosreport_var_log_directory)
+
+current_dir = os.getcwd()
+print(current_dir)
+
+absolute_path = os.path.join(current_dir, sosreport_var_log_directory)
+full_path_sosreport_var_log_directory = os.path.join(current_dir, sosreport_var_log_directory)
+print(full_path_sosreport_var_log_directory)
+
+tar_file_name  = unxz_tar_file_name.split(".xz",1)[0]
 
 untar_filename = subprocess.run(["tar","-xf",unxz_tar_file_name.split(".xz",1)[0]])
 untar_filename_status = untar_filename.returncode
@@ -48,18 +60,25 @@ if ( untar_filename_status == 0 ):
 else:
         print("tar extraction failed")
 
-xsos_execution =  subprocess.run(["/usr/local/bin/xsos","--all",unxz_tar_file_name.split(".tar",1)[0]])
-xsos_execution_status = xsos_execution.returncode
 
-if ( untar_filename_status == 0 ):
-        print("xsos analysis sucessfully created.")
-else:
-        print("xsos analysis failed")
+### Function to Uncompress the .gz file inside the /var/log #####
+def gz_extract(directory):
+    extension = ".gz"
+    os.chdir(directory)
+    for item in os.listdir(directory): # loop through items in dir
+      if item.endswith(extension): # check for ".gz" extension
+          gz_name = os.path.abspath(item) # get full path of files
+          file_name = (os.path.basename(gz_name)).rsplit('.',1)[0] #get file name for file within
+          with gzip.open(gz_name,"rb") as f_in, open(file_name,"wb") as f_out:
+              shutil.copyfileobj(f_in, f_out)
+          os.remove(gz_name) # delete zipped file
 
 #### Function to search the Error string ########
 def error_string_search(strings_to_search):
 # Find all .txt files in the current directory
-        txt_files = glob.glob('/var/log/messages*')      # Find all .txt files in the current directory
+        os.chdir(full_path_sosreport_var_log_directory)
+        # Find all .txt fils in the current directory
+        txt_files = glob.glob('messages*')
         # Iterate over the files
         for txt_file in txt_files:
             with open(txt_file) as f:
@@ -73,7 +92,7 @@ def error_string_search(strings_to_search):
                         # Print the lines containing the string
                         for line in file_contents.split('\n'):
                             if search_string in line:
-                                print(line)    
+                                print(line)
 
 
 #### Messages greping section #################
@@ -90,54 +109,68 @@ if selected_option == 1:
 
         print("You selected Option 1 , which fetch error related to file system and disk")
 
+        ## Calling Uncompress Function inside /var/log #####
+        gz_extract(full_path_sosreport_var_log_directory)
+
         # List of strings to search for
         strings_to_search = ['fsck','XFS internal error','xfs_force_shutdown','I/O error','xfs_do_force_shutdown','Corruption detected','xfs_buf_ioend','xfs_trans_read_buf','xfs_inode block','io_schedule_timeout','xfs_inode','xfs_inactive_ifree','xfs_iunlink_remove','xfs_imap_to_bp','EXT4-fs error','EXT4-fs warning','JBD: Spotted dirty metadata buffer','EXT3-fs error','blk_update_request','Buffer I/O error']
         #### Calling Function to search the Error string ####
         error_string_search(strings_to_search)
-
+        # perform action for Option 1
+        exit()
 elif selected_option == 2:
     print("You selected Option 2, which fetch error related to Memory")
-    
+
+    ## Calling Uncompress Function inside /var/log #####
+    gz_extract(full_path_sosreport_var_log_directory)
     # List of strings to search for
     strings_to_search = ['lowmem_reserve','Out of memory:','Killed process','oom-killer','check_panic_on_oom','oom_score_adj','do_page_fault']
     #### Calling Function to search the Error string ####
     error_string_search(strings_to_search)
-    
+
 
     # perform action for Option 2
-
+    exit()
 elif selected_option == 3:
     print("You selected Option 3, which fetch error related to softlock CPU and kernel panic")
-    
+
+    ## Calling Uncompress Function inside /var/log #####
+    gz_extract(full_path_sosreport_var_log_directory)
     # List of strings to search for
     strings_to_search = ['BUG: soft lockup','__do_softirq','RIP:','_raw_spin_unlock_irqrestore','unable to handle kernel paging request','unable to handle kernel NULL pointer dereference','Oops:','BUG:','PGD','Oops:','segfault at','do_page_fault','Kernel panic - not syncing','Fatal exception']
     #### Calling Function to search the Error string ####
     error_string_search(strings_to_search)
-                            
-    # perform action for Option 3
 
+    # perform action for Option 3
+    exit()
 elif selected_option == 4 :
     print("To fetch possible error related to Antivirus issues and endpoint security daemon")
-    
+
+    ## Calling Uncompress Function inside /var/log #####
+    gz_extract(full_path_sosreport_var_log_directory)
     # List of strings to search for
     strings_to_search = ['falcon_lsm_serviceable','twnotify_sys_close','twnotify','tmhook_invoke','tmhook_handler','dsa_filter','core_pkt_filter','core_pkt_hook','symev_rh_','symev_hook']
     #### Calling Function to search the Error string ####
     error_string_search(strings_to_search)
-                            
+
     # perform action for Option 4
+    exit()
 elif selected_option == 5 :
     print("To fetch all possible error related to the messages")
-    
 
+
+    ## Calling Uncompress Function inside /var/log #####
+    gz_extract(full_path_sosreport_var_log_directory)
     # List of strings to search for
     strings_to_search = ['fsck','XFS internal error','xfs_force_shutdown','I/O error','xfs_do_force_shutdown','Corruption detected','xfs_buf_ioend','xfs_trans_read_buf','xfs_inode block','io_schedule_timeout','xfs_inode','xfs_inactive_ifree','xfs_iunlink_remove','xfs_imap_to_bp','EXT4-fs error','EXT4-fs warning','JBD: Spotted dirty metadata buffer','EXT3-fs error','blk_update_request','Buffer I/O error''lowmem_reserve','Out of memory:','Killed process','oom-killer','check_panic_on_oom','oom_score_adj','do_page_fault','BUG: soft lockup','__do_softirq','RIP:','_raw_spin_unlock_irqrestore','unable to handle kernel paging request','unable to handle kernel NULL pointer dereference','Oops:','BUG:','PGD','Oops:','segfault at','do_page_fault','Kernel panic - not syncing','Fatal exception','falcon_lsm_serviceable','twnotify_sys_close','twnotify','tmhook_invoke','tmhook_handler','dsa_filter','core_pkt_filter','core_pkt_hook','symev_rh_','symev_hook']
     #### Calling Function to search the Error string ####
     error_string_search(strings_to_search)
     # perform action for Option 5
-        
+    exit()
 elif selected_option == 6 :
     print("Exit option selected to come out of this script")
     exit()
         # perform action for Option 6
 else:
     print("Invalid option selected")
+    exit()
